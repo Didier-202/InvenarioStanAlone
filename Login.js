ab26f2d6670ac48d4d506c8615dc893bf3e12ecd
Didier <herrerafercho70@gmail.com>
2026-06-01 21:11:41 -0500

    Primer commit software de inventarios

---
+++ Login.js
@@ -0,0 +1,141 @@
+// MOSTRAR Y OCULTAR SECCIONES
+function mostrarRegistro() {
+  document.getElementById("login").classList.add("hidden");  // Ocultar sección de login
+  document.getElementById("registro").classList.remove("hidden");  // Mostrar sección de registro
+}
+
+function mostrarLogin() {
+  document.getElementById("registro").classList.add("hidden"); // Ocultar sección de registro
+  document.getElementById("login").classList.remove("hidden");  // Mostrar sección de login
+}
+
+// LIMPIAR ERRORES
+
+function limpiarErrores() {
+  document.querySelectorAll(".error").forEach(e => e.textContent = "");  // Limpiar mensajes de error
+  document.querySelectorAll("input").forEach(i => i.classList.remove("errorInput")); 
+}
+
+// MOSTRAR MENSAJE DE ERROR
+function setError(id, mensaje, inputId) {  // Mostrar mensaje de error y resaltar campo
+  document.getElementById(id).textContent = mensaje;  
+  document.getElementById(inputId).classList.add("errorInput"); // Resaltar campo con error
+}
+
+// VALIDACIÓN DE CORREO
+function validarEmail(correo) {  // Expresión para validar formato de correo
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(correo); // Retorna true si el correo es válido, false si no
+}
+
+// REGISTRAR USUARIO
+function registrar() {
+  limpiarErrores();  // Limpiar mensajes de error
+
+  const nombre = document.getElementById("nombre").value.trim();  // Obtener y limpiar espacios del nombre.. y así con los demás campos
+  const documento = document.getElementById("documento").value.trim();
+  const telefono = document.getElementById("telefono").value.trim();
+  const correo = document.getElementById("correo").value.trim();
+  const user = document.getElementById("regUser").value.trim();  
+  const pass = document.getElementById("regPass").value; 
+  const confirmPass = document.getElementById("confirmPass").value; 
+
+  let valido = true;
+
+  // VALIDACIONES
+  if (!nombre) {   // Validar que el nombre no este vacio... y así con los demás campos
+    setError("errorNombre", "Ingrese su nombre", "nombre");  
+    valido = false;
+  }
+
+  if (!documento) {
+    setError("errorDocumento", "Ingrese su documento", "documento");
+    valido = false;
+  }
+
+  if (!telefono || isNaN(telefono)) {
+    setError("errorTelefono", "Teléfono inválido", "telefono");
+    valido = false;
+  }
+
+  if (!correo || !validarEmail(correo)) {
+    setError("errorCorreo", "Correo inválido", "correo");
+    valido = false;
+  }
+
+  if (!user || user.length < 4) {   // Validar que el usuario tenga al menos 4 caracteres
+    setError("errorUser", "Usuario mínimo 4 caracteres", "regUser");
+    valido = false;
+  }
+
+  if (!pass || pass.length < 6) {    // Validar que la contraseña tenga al menos 6 caracteres
+    setError("errorPass", "Contraseña mínimo 6 caracteres", "regPass");
+    valido = false;
+  }
+
+  if (pass !== confirmPass) {   // Validar que las contraseñas coincidan
+    setError("errorConfirmPass", "Las contraseñas no coinciden", "confirmPass");
+    valido = false;
+  }
+
+  if (!valido) return;
+
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];  // Obtener datos del local Storage o iniciar array vacío si no hay datos
+
+  const existe = usuarios.find(u => u.user === user);  // Verificar si el usuario ya existe en el array de usuarios
+  if (existe) {                                         // Si el usuario ya existe mostrar mensaje de error
+    setError("errorUser", "Usuario ya existe", "regUser");
+    return;
+  }
+
+  const nuevoUsuario = {  // Crear nuevo usuario con los datos del formulario
+    nombre,
+    documento,
+    telefono,
+    correo,
+    user,
+    pass
+  };
+
+  usuarios.push(nuevoUsuario);  // Agregar nuevo usuario al array de usuarios
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));  // Guardar el array actualizado en el local Storage
+
+  // MENSAJE BONITO
+  const card = document.getElementById("registro");  // Obtener referencia a la tarjeta de registro
+  card.innerHTML += `<p class="success">✅ Usuario registrado correctamente</p>`;
+
+  setTimeout(() => {  // Después de 1.5 segundos mostrar la sección de login
+    mostrarLogin();
+  }, 1500);
+}
+// LOGIN
+function login() {
+  const user = document.getElementById("loginUser").value;  // Obtener valor del campo de usuario
+  const pass = document.getElementById("loginPass").value;
+
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];  // Obtener datos del local Storage o iniciar array vacío si no hay datos
+
+  const validUser = usuarios.find(u => u.user === user && u.pass === pass);  // Verificar si existe un usuario con el mismo nombre de usuario y contraseña en el array de usuarios
+
+  if (validUser) {  // Si el usuario y contraseña son correctos guardar el usuario activo en el local Storage y redirigir al dashboard
+    localStorage.setItem("usuarioActivo", user);
+    window.location.href = "dashboard.html"; 
+    
+    // Aquí luego rediriges al sistema de inventario
+    // window.location.href = "dashboard.html";
+    
+  } else {
+    alert("Usuario o contraseña incorrectos");  // Si el usuario y contraseña no son correctos mostrar mensaje de error
+  }
+}
+
+// LIMPIAR FORMULARIO DE REGISTRO
+
+function limpiarFormularioRegistro() {  // Limpiar campos del formulario de registro 
+  document.getElementById("nombre").value = ""; // Limpiar campo de nombre... y así con los demás campos
+  document.getElementById("documento").value = "";
+  document.getElementById("telefono").value = "";
+  document.getElementById("correo").value = "";
+  document.getElementById("regUser").value = "";
+  document.getElementById("regPass").value = "";
+  document.getElementById("confirmPass").value = "";
+}
---
+++ dashboard.js
@@ -0,0 +1,90 @@
+let indiceEditar = null;
+
+
+
+// USUARIO ACTIVO
+document.getElementById("usuarioActivo").textContent =
+localStorage.getItem("usuarioActivo");
+
+// =====================
+// CARGAR USUARIOS
+// =====================
+
+function cargarUsuarios() {
+  const tabla = document.getElementById("tablaUsuarios");  // Obtener referencia a la tabla
+  tabla.innerHTML = "";
+
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];  //Obtener datos del local Storage ([vacio] si no hay datos)
+// Recorrer usuarios y mostrar en tabla
+  usuarios.forEach((u, index) => {
+    // Agregar fila a la tabla por cada usuario
+    tabla.innerHTML += `
+      <tr>
+        <td class="celdaName">${u.nombre}</td>  
+        <td>${u.documento}</td>
+        <td>${u.telefono}</td>
+        <td>${u.correo}</td>
+        <td>${u.user}</td>
+        <td class="celdaAcciones">
+          <button class="btn btn-edit" title="Editar" onclick="editarUsuario(${index})">✏️Editar</button>
+          <button class="btn btn-delete" title="Eliminar" onclick="eliminarUsuario(${index})">🗑️ Eliminar</button> 
+        </td>
+      </tr>
+    `;
+  });
+}
+
+// ELIMINAR USUARIO
+function eliminarUsuario(index) {
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];  // Obtener datos del local Storage ([] si no hay datos )
+
+  if (confirm("¿Eliminar usuario?")) {
+    usuarios.splice(index, 1);  // Eliminar usuario del array
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));  // Guardar cambios en local Storage
+    cargarUsuarios();
+  }
+}
+
+// EDITAR USUARIO
+function editarUsuario(index) {  // Obtener datos del usuario a editar
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];  // Obtener datos del local Storage ([] si no hay datos)
+  const usuario = usuarios[index];
+
+  indiceEditar = index;   
+
+  document.getElementById("editNombre").value = usuario.nombre;  // Rellenar o cambiar formulario con datos del usuario .. y asi con los demás
+  document.getElementById("editDocumento").value = usuario.documento;
+  document.getElementById("editTelefono").value = usuario.telefono;
+  document.getElementById("editCorreo").value = usuario.correo;
+
+  document.getElementById("formEditar").style.display = "block";  // Mostrar formulario de edición
+}
+
+// GUARDAR CAMBIOS DE USUARIO EDITADO
+
+function guardarCambios() {
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];  // Obtener datos del local Storage
+
+  usuarios[indiceEditar].nombre = document.getElementById("editNombre").value;  // Actualizar datos del usuario con valores del formulario
+  usuarios[indiceEditar].documento = document.getElementById("editDocumento").value;
+  usuarios[indiceEditar].telefono = document.getElementById("editTelefono").value;
+  usuarios[indiceEditar].correo = document.getElementById("editCorreo").value;
+
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  document.getElementById("formEditar").style.display = "none";   // Ocultar formulario de edición
+  cargarUsuarios();
+}
+
+
+// =====================
+// SESIÓN
+// =====================
+
+
+// INICIO
+cargarUsuarios();
+cargarProductos();
+mostrarModulo('usuarios');
+
+
+
